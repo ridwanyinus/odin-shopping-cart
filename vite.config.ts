88f630f4a3229d979a5f5c999734c2161b04738f
ridwan <ridwanyinusa15@gmail.com>
2025-08-18 11:55:35 +0100

    add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import viteReact from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteReact()],
+ test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@': '/src',
